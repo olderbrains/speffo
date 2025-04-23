cd03c61fda0d29c04d94ec72fef70ec0450ca6ef
simplyfy <developer.amrit.randhwa@gmail.com>
2025-04-23 23:47:26 +0530

    April 23 2025

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:lottie/lottie.dart';
 import 'package:speffo/Themes/text_theme.dart';
 
 void main() {
@@ -18,12 +19,45 @@ class SpeffoApp extends StatefulWidget {
   State<SpeffoApp> createState() => _SpeffoAppState();
 }
 
-class _SpeffoAppState extends State<SpeffoApp> {
+class _SpeffoAppState extends State<SpeffoApp>   with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: globalTheme(),
-      home: Scaffold(body: Center(child: Image.asset('assets/logo/logo.png'))),
+      home: Scaffold(body: Center(child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset('assets/logo/logo.png',height: 75.h,),
+
+          Lottie.asset(
+            'assets/lottie/splashLottie.json',
+            height: 100.h,
+            controller: _controller,
+
+            onLoaded: (composition) {
+              _controller
+                ..duration = composition.duration
+                ..repeat();
+            },
+          ),
+        ],
+      ))),
     );
   }
 }
