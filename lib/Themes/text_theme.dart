a50b74e32d54448cc40828b42fcd7be724f558b2
simplyfy <developer.amrit.randhwa@gmail.com>
2025-04-23 22:51:39 +0530

    23 April 2025

---
+++ lib/Themes/text_theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+ThemeData globalTheme() {
+  return ThemeData(
+
+    fontFamily: 'NotoSans',
+    textTheme: const TextTheme(
+      displayLarge: TextStyle(fontWeight: FontWeight.w900),
+      displayMedium: TextStyle(fontWeight: FontWeight.w800),
+      displaySmall: TextStyle(fontWeight: FontWeight.w700),
+      headlineMedium: TextStyle(fontWeight: FontWeight.w600),
+      headlineSmall: TextStyle(fontWeight: FontWeight.w500),
+      titleLarge: TextStyle(fontWeight: FontWeight.w400),
+      titleMedium: TextStyle(fontWeight: FontWeight.w300),
+      titleSmall: TextStyle(fontWeight: FontWeight.w200),
+      bodyLarge: TextStyle(fontWeight: FontWeight.w100),
+    ),
+  );
+}
