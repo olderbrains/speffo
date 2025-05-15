66d39b862af6d8deb63100ad88b0289a100886f3
Amrit Randhawa <amrit@simplyfy.co.in>
2025-05-15 23:07:51 +0530

    second try

--- lib/Helper/api_url.dart
+++ lib/Helper/api_url.dart
@@ -1,5 +1,5 @@
 class ApiURl {
-  static String baseUrl = "https://api.olderbrains.com/";
+  static String baseUrl = "https://sandbox.olderbrains.com/";
   static String createFirebaseUser = "api/auth/createFirebaseUser";
   static String sentOTPUrl = "https://control.msg91.com/api/v5/otp?";
   static String verifyOTPUrl = "https://control.msg91.com/api/v5/otp/verify?";
